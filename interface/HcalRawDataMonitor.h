c1b0328bb805a15f13bbb76cc319926d68b86498
Jason St John <sha1-13e6ca3f617e2679b2e43c5867c3ac26779e685c@cern.ch>
2010-03-02 20:59:46 +0000

    Complete contructor functionality, beginning setup() functionality.

--- interface/HcalRawDataMonitor.h
+++ interface/HcalRawDataMonitor.h
@@ -1,5 +1,28 @@
 #ifndef DQM_HCALMONITORTASKS_HCALRAWDATAMONITOR_H
 #define DQM_HCALMONITORTASKS_HCALRAWDATAMONITOR_H
+
+#define DEPTHBINS      4
+#define  IETAMIN     -43
+#define  IETAMAX      43
+#define  IPHIMIN       0
+#define  IPHIMAX      71
+#define  HBHE_LO_DCC 700
+#define  HBHE_HI_DCC 717
+#define  HF_LO_DCC   718
+#define  HF_HI_DCC   724
+#define  HO_LO_DCC   725
+#define  HO_HI_DCC   731
+#define  NUMDCCS      32
+#define  NUMSPIGS     15
+#define  HTRCHANMAX   24
+//Dimensions of 'LED' plots, grouping bits by hardware space
+//  NUMBER_HDWARE = 1 + ((NUMBER +1)*(NUM_HRDWARE_PIECES))
+#define  TWO___FED   (1+((2+1)*NUMDCCS)   )
+#define  THREE_FED   (1+((3+1)*NUMDCCS)   )
+#define  TWO__SPGT   (1+((2+1)*NUMSPIGS)  )
+#define  THREE_SPG   (1+((3+1)*NUMSPIGS)  ) 
+#define  TWO_CHANN   (1+((2+1)*HTRCHANMAX))
+
 #include "DQM/HcalMonitorTasks/interface/HcalBaseDQMonitor.h"
 #include "EventFilter/HcalRawToDigi/interface/HcalUnpacker.h"
 #include "EventFilter/HcalRawToDigi/interface/HcalHTRData.h"
@@ -11,8 +34,8 @@
 
 /** \class HcalRawDataMonitor
  *
- * $Date: 2010/03/01 14:43:00 $
- * $Revision: 1.1 $
+ * $Date: 2010/03/01 23:52:57 $
+ * $Revision: 1.1.2.1 $
  * \author J. St. John - Boston University
  */
 class HcalRawDataMonitor: public HcalBaseDQMonitor {
@@ -21,7 +44,7 @@ class HcalRawDataMonitor: public HcalBaseDQMonitor {
   //Constructor with no arguments
   HcalRawDataMonitor(){};
   ~HcalRawDataMonitor();
-protected:
+ protected:
 
   // Analyze
   void analyze(const edm::Event& e, const edm::EventSetup& c);
@@ -40,7 +63,7 @@ protected:
   void endLuminosityBlock(const edm::LuminosityBlock& lumiSeg,
                           const edm::EventSetup& c);
 
- // EndJob
+  // EndJob
   void endJob(void);
 
   // EndRun
@@ -84,6 +107,50 @@ protected:
   MonitorElement *ProblemsVsLB_HO, *ProblemsVsLB_HF;
 
   int NLumiBlocks_;
+
+ private:
+  MonitorElement* meCh_DataIntegrityFED00_;   //DataIntegrity for channels in FED 00
+  MonitorElement* meCh_DataIntegrityFED01_;   //DataIntegrity for channels in FED 01
+  MonitorElement* meCh_DataIntegrityFED02_;   //DataIntegrity for channels in FED 02
+  MonitorElement* meCh_DataIntegrityFED03_;   //DataIntegrity for channels in FED 03
+  MonitorElement* meCh_DataIntegrityFED04_;   //DataIntegrity for channels in FED 04
+  MonitorElement* meCh_DataIntegrityFED05_;   //DataIntegrity for channels in FED 05
+  MonitorElement* meCh_DataIntegrityFED06_;   //DataIntegrity for channels in FED 06
+  MonitorElement* meCh_DataIntegrityFED07_;   //DataIntegrity for channels in FED 07
+  MonitorElement* meCh_DataIntegrityFED08_;   //DataIntegrity for channels in FED 08
+  MonitorElement* meCh_DataIntegrityFED09_;   //DataIntegrity for channels in FED 09
+  MonitorElement* meCh_DataIntegrityFED10_;   //DataIntegrity for channels in FED 10
+  MonitorElement* meCh_DataIntegrityFED11_;   //DataIntegrity for channels in FED 11
+  MonitorElement* meCh_DataIntegrityFED12_;   //DataIntegrity for channels in FED 12
+  MonitorElement* meCh_DataIntegrityFED13_;   //DataIntegrity for channels in FED 13
+  MonitorElement* meCh_DataIntegrityFED14_;   //DataIntegrity for channels in FED 14
+  MonitorElement* meCh_DataIntegrityFED15_;   //DataIntegrity for channels in FED 15
+  MonitorElement* meCh_DataIntegrityFED16_;   //DataIntegrity for channels in FED 16
+  MonitorElement* meCh_DataIntegrityFED17_;   //DataIntegrity for channels in FED 17
+  MonitorElement* meCh_DataIntegrityFED18_;   //DataIntegrity for channels in FED 18
+  MonitorElement* meCh_DataIntegrityFED19_;   //DataIntegrity for channels in FED 19
+  MonitorElement* meCh_DataIntegrityFED20_;   //DataIntegrity for channels in FED 20
+  MonitorElement* meCh_DataIntegrityFED21_;   //DataIntegrity for channels in FED 21
+  MonitorElement* meCh_DataIntegrityFED22_;   //DataIntegrity for channels in FED 22
+  MonitorElement* meCh_DataIntegrityFED23_;   //DataIntegrity for channels in FED 23
+  MonitorElement* meCh_DataIntegrityFED24_;   //DataIntegrity for channels in FED 24
+  MonitorElement* meCh_DataIntegrityFED25_;   //DataIntegrity for channels in FED 25
+  MonitorElement* meCh_DataIntegrityFED26_;   //DataIntegrity for channels in FED 26
+  MonitorElement* meCh_DataIntegrityFED27_;   //DataIntegrity for channels in FED 27
+  MonitorElement* meCh_DataIntegrityFED28_;   //DataIntegrity for channels in FED 28
+  MonitorElement* meCh_DataIntegrityFED29_;   //DataIntegrity for channels in FED 29
+  MonitorElement* meCh_DataIntegrityFED30_;   //DataIntegrity for channels in FED 30
+  MonitorElement* meCh_DataIntegrityFED31_;   //DataIntegrity for channels in FED 31
+  // handy array of pointers to pointers...
+  MonitorElement* meChann_DataIntegrityCheck_[NUMDCCS];
+
+  uint64_t UScount[NUMDCCS][NUMSPIGS];
+  float HalfHTRDataCorruptionIndicators_  [THREE_FED][THREE_SPG];  
+  float LRBDataCorruptionIndicators_      [THREE_FED][THREE_SPG];  
+  float ChannSumm_DataIntegrityCheck_     [TWO___FED][TWO__SPGT];
+  float Chann_DataIntegrityCheck_[NUMDCCS][TWO_CHANN][TWO__SPGT];
+
+  HcalDetId hashedHcalDetId_[NUMDCCS * NUMSPIGS * HTRCHANMAX];
   
 };
 
--- src/HcalRawDataMonitor.cc
+++ src/HcalRawDataMonitor.cc
@@ -19,38 +19,104 @@ HcalRawDataMonitor::HcalRawDataMonitor(const edm::ParameterSet& ps) {
   skipOutOfOrderLS_      = ps.getParameter<bool>("skipOutOfOrderLS");
   NLumiBlocks_           = ps.getParameter<int>("NLumiBlocks");
   makeDiagnostics_       = ps.getUntrackedParameter<bool>("makeDiagnostics",false);
+
+  // Initialize an array of MonitorElements
+  meChann_DataIntegrityCheck_[0] =meCh_DataIntegrityFED00_;
+  meChann_DataIntegrityCheck_[1] =meCh_DataIntegrityFED01_;
+  meChann_DataIntegrityCheck_[2] =meCh_DataIntegrityFED02_;
+  meChann_DataIntegrityCheck_[3] =meCh_DataIntegrityFED03_;
+  meChann_DataIntegrityCheck_[4] =meCh_DataIntegrityFED04_;
+  meChann_DataIntegrityCheck_[5] =meCh_DataIntegrityFED05_;
+  meChann_DataIntegrityCheck_[6] =meCh_DataIntegrityFED06_;
+  meChann_DataIntegrityCheck_[7] =meCh_DataIntegrityFED07_;
+  meChann_DataIntegrityCheck_[8] =meCh_DataIntegrityFED08_;
+  meChann_DataIntegrityCheck_[9] =meCh_DataIntegrityFED09_;
+  meChann_DataIntegrityCheck_[10]=meCh_DataIntegrityFED10_;
+  meChann_DataIntegrityCheck_[11]=meCh_DataIntegrityFED11_;
+  meChann_DataIntegrityCheck_[12]=meCh_DataIntegrityFED12_;
+  meChann_DataIntegrityCheck_[13]=meCh_DataIntegrityFED13_;
+  meChann_DataIntegrityCheck_[14]=meCh_DataIntegrityFED14_;
+  meChann_DataIntegrityCheck_[15]=meCh_DataIntegrityFED15_;
+  meChann_DataIntegrityCheck_[16]=meCh_DataIntegrityFED16_;
+  meChann_DataIntegrityCheck_[17]=meCh_DataIntegrityFED17_;
+  meChann_DataIntegrityCheck_[18]=meCh_DataIntegrityFED18_;
+  meChann_DataIntegrityCheck_[19]=meCh_DataIntegrityFED19_;
+  meChann_DataIntegrityCheck_[20]=meCh_DataIntegrityFED20_;
+  meChann_DataIntegrityCheck_[21]=meCh_DataIntegrityFED21_;
+  meChann_DataIntegrityCheck_[22]=meCh_DataIntegrityFED22_;
+  meChann_DataIntegrityCheck_[23]=meCh_DataIntegrityFED23_;
+  meChann_DataIntegrityCheck_[24]=meCh_DataIntegrityFED24_;
+  meChann_DataIntegrityCheck_[25]=meCh_DataIntegrityFED25_;
+  meChann_DataIntegrityCheck_[26]=meCh_DataIntegrityFED26_;
+  meChann_DataIntegrityCheck_[27]=meCh_DataIntegrityFED27_;
+  meChann_DataIntegrityCheck_[28]=meCh_DataIntegrityFED28_;
+  meChann_DataIntegrityCheck_[29]=meCh_DataIntegrityFED29_;
+  meChann_DataIntegrityCheck_[30]=meCh_DataIntegrityFED30_;
+  meChann_DataIntegrityCheck_[31]=meCh_DataIntegrityFED31_;
+
+  for (int f=0; f<NUMDCCS; f++) {
+    for (int s=0; s<15; s++) {
+      UScount[f][s]=0;}}
+
+  for (int x=0; x<THREE_FED; x++)
+    for (int y=0; y<THREE_SPG; y++)
+      HalfHTRDataCorruptionIndicators_  [x][y]=0;
+
+  for (int x=0; x<THREE_FED; x++)
+    for (int y=0; y<THREE_SPG; y++)
+      LRBDataCorruptionIndicators_  [x][y]=0;
+  	 
+  for (int x=0; x<TWO___FED; x++)
+    for (int y=0; y<TWO__SPGT; y++)
+      ChannSumm_DataIntegrityCheck_[x][y]=0;
+
+  for (int f=0; f<NUMDCCS; f++)
+    for (int x=0; x<  TWO_CHANN; x++)
+      for (int y=0; y<TWO__SPGT; y++)      
+	Chann_DataIntegrityCheck_  [f][x][y]=0;
+
+  for (int i=0; i<(NUMDCCS * NUMSPIGS * HTRCHANMAX); i++) 
+    hashedHcalDetId_[i]=HcalDetId::Undefined;
+
 } // HcalRawDataMonitor::HcalRawDataMonitor()
 
 // destructor
 HcalRawDataMonitor::~HcalRawDataMonitor(){}
 
-  // Analyze
-void HcalRawDataMonitor::analyze(const edm::Event& e, const edm::EventSetup& c){}
-
-// Setup
-void HcalRawDataMonitor::setup(void){}
-
-
-  // BeginJob
+// BeginJob
 void HcalRawDataMonitor::beginJob(){}
 
-  // BeginRun
+// BeginRun
 void HcalRawDataMonitor::beginRun(const edm::Run& run, const edm::EventSetup& c){}
 
-  // Begin LumiBlock
+// Begin LumiBlock
 void HcalRawDataMonitor::beginLuminosityBlock(const edm::LuminosityBlock& lumiSeg,
-                            const edm::EventSetup& c) {}
+					      const edm::EventSetup& c) {}
+// Setup
+void HcalRawDataMonitor::setup(void){
+  // Call base class setup
+  HcalBaseDQMonitor::setup();
+  if (!dbe_) return;
 
-  // End LumiBlock
+  /******* Set up all histograms  ********/
+  if (debug_>1)
+    std::cout <<"<HcalRawDataMonitor::beginRun>  Setting up histograms"<<endl;
+}
+
+// Analyze
+void HcalRawDataMonitor::analyze(const edm::Event& e, const edm::EventSetup& c){}
+
+// End LumiBlock
 void HcalRawDataMonitor::endLuminosityBlock(const edm::LuminosityBlock& lumiSeg,
-                          const edm::EventSetup& c){}
-
- // EndJob
-void HcalRawDataMonitor::endJob(void){}
-
-  // EndRun
+					    const edm::EventSetup& c){}
+// EndRun -- Anything to do here?
 void HcalRawDataMonitor::endRun(const edm::Run& run, const edm::EventSetup& c){}
 
+// EndJob
+void HcalRawDataMonitor::endJob(void){
+  if (debug_>0) std::cout <<"HcalRawDataMonitor::endJob()"<<std::endl;
+  if (enableCleanup_) cleanup(); // when do we force cleanup?
+}
 
 
 DEFINE_ANOTHER_FWK_MODULE(HcalRawDataMonitor);
